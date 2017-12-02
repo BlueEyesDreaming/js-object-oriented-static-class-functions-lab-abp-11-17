ede23713d66c2aadae578e1cccad7b299bbd0012
Ashton Reed <dreamergirl411@gmail.com>
2017-12-02 19:40:45 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,28 @@
 'use strict';
+
+class Question {
+  constructor(content) {
+    this.content = content
+    this.save()
+  }
+
+  save() {
+    this.constructor._All.push(this)
+  }
+
+  static All() {
+    return this._All
+  }
+
+  static Find(id) {
+    return this.All()[id - 1]
+  }
+
+    // return this.All().filter(function(questions){
+    //   return questions.id === id
+    // })
+  }
+
+
+
+Question._All = [];
